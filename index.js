8dcc5cdf2cc48fcced059a6642d19dd9be77533c
Divyank Shah <divyank.shah.2016@gmail.com>
2023-12-18 17:33:20 -0800

    add files for linting action

---
+++ index.js
@@ -0,0 +1,21 @@
+import { setFailed, info, error } from "@actions/core";
+import { getExecOutput } from "@actions/exec";
+
+const linting = async () => {
+  const { stdout } = await getExecOutput("npm i eslint -D");
+  info(stdout);
+
+  try {
+    const { stdout } = await getExecOutput(
+      "npx eslint . --ext js --ext ts --ext jsx --ext tsx"
+    );
+    info(stdout);
+  } catch (err) {
+    error(err);
+    setFailed(
+      "Your code is not linted correctly. Please format using `npm run eslint` or `npx eslint . --ext js --ext ts --ext jsx --ext tsx` and address the problems"
+    );
+  }
+};
+
+await linting();
